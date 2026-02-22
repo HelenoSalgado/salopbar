c4066fe5973bd4602cb76c102b86b098ebaa40bd
Heleno Salgado <helenosalgado19@gmail.com>
2026-02-22 01:26:54 -0300

    Correção de largura do rótulo de exibição de nomes de arquivos ou aplicativos

--- src/components/FocusClient.tsx
+++ src/components/FocusClient.tsx
@@ -1,17 +1,24 @@
 import { createBinding } from "ags";
+import { Gtk } from "ags/gtk4";
 import Hyprland from "gi://AstalHyprland?version=0.1";
+import Pango from "gi://Pango";
 
 export default function FocusedClient() {
   const hypr = Hyprland.get_default();
   const focused = createBinding(hypr, "focusedClient");
   const title = focused.as((client) => {
-    return client.title;
+    return client ? client.title : "";
   });
   
   return <box
     visible
     cssClasses={["focused"]}
   >
-    <label label={title}/>
+    <label 
+      label={title}
+      maxWidthChars={50}
+      ellipsize={Pango.EllipsizeMode.END}
+      halign={Gtk.Align.CENTER}
+    />
   </box>
 }
